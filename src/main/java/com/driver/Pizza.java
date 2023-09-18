c89cdfcbd2fc5ea788020dd17ccf46fa0af4d34e
Mahesh Misal <maheshmisal2018@gmail.com>
2023-09-18 11:51:36 +0530

    comleted

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,75 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+    private int price = 0;
+    private Boolean isVeg = true;
+    private String bill = "";
+    private int pizza = 0;
+    private int extraCheese = 0;
+    private int toppings = 0;
+    private int paperBag = 0;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg)
+        {
+            pizza = 300;
+        }
+        else
+        {
+            pizza = 400;
+        }
+        price += pizza;
     }
 
     public int getPrice(){
         return this.price;
     }
 
+    public int getPizza() {
+        return pizza;
+    }
+    public int getExtraCheese() {
+        return extraCheese;
+    }
+
+    public int getToppings() {
+        return toppings;
+    }
+
+    public int getPaperBag() {
+        return paperBag;
+    }
+
     public void addExtraCheese(){
         // your code goes here
+        extraCheese = 80;
+        price += extraCheese;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isVeg)
+            toppings = 70;
+        else
+            toppings = 120;
+
+        price += toppings;
     }
 
     public void addTakeaway(){
-        // your code goes here
+        paperBag = 20;
+        price += paperBag;
     }
 
     public String getBill(){
-        // your code goes here
+            bill += "Base Price Of The Pizza: "+pizza+ "\n";
+            if(extraCheese != 0)
+                bill += "Extra Cheese Added: "+extraCheese+ "\n";
+            if(toppings != 0)
+                bill += "Extra Toppings Added: "+toppings+ "\n";
+            if(toppings != 0)
+                bill += "Paperbag Added: "+paperBag+ "\n";
+            bill += "Total Price: "+price;
         return this.bill;
     }
 }
